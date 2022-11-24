d566c6128638b422835697c03de0ca5b85470b84
Xavier <gemparpanggih123@gmail.com>
2022-11-24 22:06:06 +0800

    landing page awal

--- index.php
+++ index.php
@@ -23,167 +23,160 @@
     <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
     <link href="//db.onlinewebfonts.com/c/213e56f9ea368890b9d2da0577e49dab?family=Zona+Pro" rel="stylesheet" type="text/css"/>
-    <script src="https://kit.fontawesome.com/32f82e1dca.js" crossorigin="anonymous"></script>
 
-    <!-- Link Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"/>
-    
     <!-- CSS -->
-    <link rel="stylesheet" href="stylesheet/nonavbar.css">
-    <link rel="stylesheet" href="stylesheet/landingPage.css">
+    <link rel="stylesheet" href="stylesheet/style.css">
+    <link rel="stylesheet" href="stylesheet/style-mobile.css">
 </head>
-
 <body>
-    <!-- SIDEBAR -->
-    <aside class="sidebar offcanvas-lg offcanvas-start">
-        <div class="d-flex justify-content-end m-4 d-block d-md-none">
-            <button data-bs-dismiss="offcanvas" data-bs-target=".sidebar" class="btn p-0 border-0 fs-4" aria-label="Button Close">
-                <i class="fas fa-close"></i>
-            </button>
-        </div>
-
-        <div class="logo-brand mt-lg-5">
-            <img src="img/logo/logo-listrik.png" alt="" width="48" height="50"/>
-            <div>
-                <h6 class="title">Listrik Biru</h6>
-                <p class="tagline">To Brigthen Your Home</p>
-            </div>
-        </div>
-
-        <hr/>
-        <nav class="menu flex-fill">
-            <div class="section-menu">
-                <a href="tarif.php" class="item-menu active" onclick="handleClickMenu(this)">
-                    <p>Tarif</p>
+    <!-- HEADER -->
+    <header class="main" id="home">
+        <!-- navbar -->
+        <nav class="mode-bg">
+            <div class="logo">
+                <a href="index.php" class="mode-text">
+                    <img src="img/logo/logo-listrik.png" alt="">
+                    <p>Listrik Biru</p>
                 </a>
+            </div>
+
+            <ul>
+                <li><a class="mode-text" href="tarif.php">Tarif</a></li>
                 <?php if($_SESSION['akun']['level'] == 'admin') { ?>
-                    <a href="admin/transaksi.php" class="item-menu" onclick="handleClickMenu(this)">
-                        <p>Transaksi</p>
-                    </a>
-
-                    <a href="admin/pelanggan.php" class="item-menu" onclick="handleClickMenu(this)">
-                        <p>Pelanggan</p>
-                    </a>
-
-                    <a href="admin/daftar-pesan.php" class="item-menu" onclick="handleClickMenu(this)">
-                        <p>Kontak</p>
-                    </a>
-
-
+                    <li><a class="mode-text" href="admin/transaksi.php">Transaksi</a></li>
+                    <li><a class="mode-text" href="admin/pelanggan.php">Pelanggan</a></li>
+                    <li><a class="mode-text" href="admin/daftar-pesan.php">Kontak</a></li>
                 <?php } if($_SESSION['akun']['level'] == 'user') { ?>
-                    <a href="user/profil.php" class="item-menu" onclick="handleClickMenu(this)">
-                        <p>Profil</p>
-                    </a>
-
-                    <a href="user/kontak.ph" class="item-menu" onclick="handleClickMenu(this)">
-                        <p>Kontak</p>
-                    </a>
+                    <li><a class="mode-text" href="user/profil.php">Profil</a></li>
+                    <li><a class="mode-text" href="user/kontak.php">Kontak</a></li>
                 <?php } ?>
+                <div class="logout-btn">
+                    <a href="auth/logout.php">Logout</a>
+                </div>
+            </ul>
+            <div class="dark-mode-toggle">
+                <input type="checkbox" class="checkbox" id="chk"/>
+                <label class="label" for="chk">
+                    <i class="fas fa-moon"></i>
+                    <i class="fas fa-sun"></i>
+                    <div class="ball"></div>
+                </label>
+            </div>
+
+            <div class="menu-toggle">
+                <input type="checkbox" id="menTog"/>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
         </nav>
+        <!-- end navbar -->
 
-        <footer>
-            <div class="d-flex align-items-center mb-4">
-                <img src="./assets/icons/ic_mode.svg" alt="Mode Display" />
-                <p id="label-mode" class="flex-fill label-mode">Light Mode</p>
-
-                <div>
-                    <input id="checkbox" type="checkbox" class="toggle-theme" aria-label="Toggle Theme"/>
-                    <label for="checkbox" class="label-toggle">
-                        <img src="./assets/icons/ic_moon.svg" width="50%" class="ic-theme" id="ic-dark" alt="Icon Dark"/>
-                        <img src="./assets/icons/ic_sun.svg"  width="50%" class="ic-theme" id="ic-light" alt="Icon Light"/>
-                    </label>
-                </div>
-            </div>
-
-            <div class="logout-btn">
-                <a href="auth/logout.php">Logout</a>
-            </div>
-        </footer>
-    </aside>
-
-    <main class="content flex-fill">
-        <section>
-            <button aria-controls="sidebar" data-bs-toggle="offcanvas" data-bs-target=".sidebar" aria-label="Button Hamburger" class="sidebarOffcanvas mb-5 btn p-0 border-0 d-flex d-lg-none">
-            <i class="fa-solid fa-bars"></i>
-            </button>
-        </section>
-
-        <section class="d-flex flex-column">
-            <div class="header-container">
-                <div class="header-item-left">
-                    <div class="pesan-container">
-                        <?php if(isset($_GET['pesan'])) {?>
-                            <div class="pesan-sukses"> <?php echo $_GET['pesan']; ?> </div>
-                        <?php } ?>
-                    </div>
-                    <h1>LISTRIK BIRU</h1>
-                    <p><b>Listrik Biru</b> 
-                    Pulsa listrik di rumah mau habis tapi belum beli token listrik karena males keluar? Tenang, kini Anda bisa beli token listrik prabayar dan tagihan listrik pascabayar dengan cepat dan mudah bisa di mana saja dan kapan saja hanya dengan satu aplikasi.
-                    <br>
-                    Pulsa listrik di rumah sudah mau habis tapi malas keluar untuk beli token listrik prabayar? Mau beli token listrik prabayar dengan cepat dan mudah? Yuk beli token listrik prabayar murah dengan proses yang aman dan cepat hanya di <b>Listrik Biru</b>!</p>
-                    
-                    <a href="user/pembelian.php">
-                        <button class="rent-btn">Pesan!</button>
-                    </a>
-
-                    <a href="tarif.php">
-                        <button class="play-btn">Tarif</button>
-                    </a>
-                </div>
-                <div class="header-item-right"></div>
-            </div>
-
-            <section>
-                <div class="about-container mode-bg mode-text" id="about">
-                    <div class="about-title">
-                        <h2>ABOUT US</h2>
-                    </div>
-                    <div class="about-content">
-                        <p class="about-item-left"><b>Listrik Biru</b> adalah layanan prabayar listrik online Samarinda milik swasta yang berspesialisasi dalam transaksi listrik untuk sistem, mulai dari generasi keenam dan seterusnya. Model bisnis <b>Listrik Biru</b> mirip dengan layanan dalam membeli pulsa secara online dan langganan secara online. <b>Listrik Biru</b> mengirimkan token listrik ke pelanggan dengan biaya yang ditetapkan.</p>
-                        <p class="about-item-right">Lebih dari 1 tarif tersedia. Pada Mei 2018, Electronic Arts mengumumkan bahwa mereka mengakuisisi aset dan personel teknologi cloud dari <b>Listrik Biru</b> (termasuk pos terdepan Chicago). <b>Listrik Biru</b> saat ini dimiliki oleh grup kepemilikan yang sama dengan Alliance Entertainment dan dioperasikan sebagai perusahaan yang berdiri sendiri.</p>
-                    </div>
-                </div>   
-            </section>
-
-            <div class="service-container">
-                <h3>Daftar Tarif</h3>
-                <div class="service-cards">
-                    <?php
-                        require('php/connection.php');
-                        $sql="SELECT * FROM tarif";
-                        $query= mysqli_query($conn, $sql);
-                        while ($data = mysqli_fetch_array($query)) { 
-                    ?>
-                        <div class="card-panel mode-bg mode-text" onclick="window.location='tarif.php'">
-                            <div class="grid-container">
-                                <div>
-                                    <img src="img/voltase/<?php echo $data['foto'] ?>"/>
-                                    <p>
-                                        <?php echo "$data[id]"; ?> ||
-                                        <?php echo "$data[daya]"; ?><br>
-                                    </p>
-                                    <p>
-                                        Rp.<?php echo "$data[tarifperkwh]"; ?>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+        <div class="header-container">
+            <div class="header-item-left">
+                <div class="pesan-container">
+                    <?php if(isset($_GET['pesan'])) {?>
+                        <div class="pesan-sukses"> <?php echo $_GET['pesan']; ?> </div>
                     <?php } ?>
                 </div>
+                <h1>LISTRIK BIRU</h1>
+                <p><b>Listrik Biru</b> Pulsa listrik di rumah mau habis tapi belum beli token listrik karena males keluar? Tenang, kini Anda bisa beli token listrik prabayar dan tagihan listrik pascabayar dengan cepat dan mudah bisa di mana saja dan kapan saja hanya dengan satu aplikasi.
+                <br>
+                Pulsa listrik di rumah sudah mau habis tapi malas keluar untuk beli token listrik prabayar? Mau beli token listrik prabayar dengan cepat dan mudah? Yuk beli token listrik prabayar murah dengan proses yang aman dan cepat hanya di <b>Listrik Biru</b>!</p>
+                <a href="user/pembelian.php">
+                    <button class="rent-btn">Pesan!</button>
+                </a>
+                <a href="tarif.php">
+                    <button class="play-btn">Tarif</button>
+                </a>
             </div>
-        </section>
+            <div class="header-item-right">
+            </div>
+        </div>
+    </header>
+    <!-- END HEADER -->
 
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-        crossorigin="anonymous"
-    ></script>
+
+    <!-- MAIN CONTENT -->
+    <section>
+        <div class="about-container mode-bg mode-text" id="about">
+            <div class="about-title">
+                <h2>ABOUT US</h2>
+            </div>
+            <div class="about-content">
+                <p class="about-item-left"><b>Listrik Biru</b> adalah layanan prabayar listrik online Samarinda milik swasta yang berspesialisasi dalam transaksi listrik untuk sistem, mulai dari generasi keenam dan seterusnya. Model bisnis <b>Listrik Biru</b> mirip dengan layanan dalam membeli pulsa secara online dan langganan secara online. <b>Listrik Biru</b> mengirimkan token listrik ke pelanggan dengan biaya yang ditetapkan.</p>
+                <p class="about-item-right">Lebih dari 1 tarif tersedia. Pada Mei 2018, Electronic Arts mengumumkan bahwa mereka mengakuisisi aset dan personel teknologi cloud dari <b>Listrik Biru</b> (termasuk pos terdepan Chicago). <b>Listrik Biru</b> saat ini dimiliki oleh grup kepemilikan yang sama dengan Alliance Entertainment dan dioperasikan sebagai perusahaan yang berdiri sendiri.</p>
+            </div>
+        </div>
+
+        <div class="service-container">
+            <h3>Daftar Tarif</h3>
+            <div class="service-cards">
+                <?php
+                    require('php/connection.php');
+                    $sql="SELECT * FROM tarif";
+                    $query= mysqli_query($conn, $sql);
+                    while ($data = mysqli_fetch_array($query)) { 
+                ?>
+                    <div class="card-panel mode-bg mode-text" onclick="window.location='tarif.php'">
+                        <div class="grid-container">
+                            <div>
+                                <img src="img/voltase/<?php echo $data['foto'] ?>"/>
+                                <p>
+                                    <?php echo "$data[id]"; ?> ||
+                                    <?php echo "$data[daya]"; ?><br>
+                                </p>
+                                <p>
+                                    Rp.<?php echo "$data[tarifperkwh]"; ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </section>
+    <!-- END MAIN CONTENT -->
+
+
+    <!-- FOOTER -->
+    <footer class="mode-bg" style="position: static;">
+        <div class="footer-container">
+            <div class="footer-title" id="contact">
+                <h2>CONTACT US</h2>
+            </div>
+            <div class="footer-contact-item">
+                <div class="footer-item">
+                    <h4>Location</h4>
+                    <p>28 Jackson Blvd Ste 1020 Chicago<br>IL 60604-2340<br>Phone: +628 135 158 0524</p>
+                </div>
+                <div class="footer-item">
+                    <h4>Find Us On</h4>
+                    <div class="circle-container">
+                        <!-- salah satu fitur pop up box (confirm) -->
+                        <div class="circle ig">
+                            <a href="https://www.instagram.com/listrik-biru" onclick="return confirm('You will be redirected to other website.');"><i class="fa-brands fa-instagram"></i></a>
+                        </div>
+                        <div class="circle fb">
+                            <a href="https://www.facebook.com/listrik-biru" onclick="return confirm('You will be redirected to other website.');"><i class="fa-brands fa-facebook"></i></a>
+                        </div>
+                        <div class="circle wa">
+                            <a href="https://www.whatsapp.com/listrik-biru" onclick="return confirm('You will be redirected to other website.');"><i class="fa-brands fa-whatsapp"></i></a>
+                        </div>
+                        <div class="circle tw">
+                            <a href="https://www.twitter.com/listrik-biru" onclick="return confirm('You will be redirected to other website.');"><i class="fa-brands fa-twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- END FOOTER -->
 
     <!-- javascript -->
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
-    <script src="js/index.js"></script>
     <script src="js/dark-mode.js"></script>
-
+    <script src="js/navbar-mobile.js"></script>
+    <script src="https://kit.fontawesome.com/a374d5ed26.js" crossorigin="anonymous"></script>
 </body>
 </html>
