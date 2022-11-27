d991fe2bc8403036bfabb956beec04dfcc2f8491
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-11-27 21:03:42 +0800

    tarif

--- tarif.php
+++ tarif.php
@@ -20,99 +20,111 @@
     <link rel="shortcut icon" href="img/logo/logo-listrik.png">
     
     <!-- Link Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-    <link href="//db.onlinewebfonts.com/c/213e56f9ea368890b9d2da0577e49dab?family=Zona+Pro" rel="stylesheet" type="text/css"/>
+    <link href="//db.onlinewebfonts.com/c/213e56f9ea368890b9d2da0577e49dab?family=Zona+Pro" rel="stylesheet" type="text/css"/> -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 
     <!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="stylesheet/tarif.css">
-    <link rel="stylesheet" href="stylesheet/style.css">
-    <link rel="stylesheet" href="stylesheet/style-mobile.css">
+    <link rel="stylesheet" href="stylesheet/new/main.css">
+    <link rel="stylesheet" href="stylesheet/component/sidebar.css">
+    <link rel="stylesheet" href="stylesheet/nonavbar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body class="mode-bg">
+<body id="taif">
     <!-- HEADER -->
-    <header class="main" id="home">
-        <!-- navbar -->
-        <nav class="mode-bg">
-            <div class="logo">
-                <a href="index.php" class="mode-text">
-                    <img src="img/logo/logo-listrik.png" alt="">
-                    <p>Listrik Biru</p>
-                </a>
-            </div>
+    
+    <!-- Sidebar -->
+    <aside class="sidebar offcanvas-lg offcanvas-start mode-bg">
+        <div class="d-flex  m-4 d-block d-lg-none mode-bg">
+            <button data-bs-dismiss="offcanvas" data-bs-target=".sidebar" class="btn p-0 border-0 fs-4" aria-label="Button Close">
+            <i class="fas fa-close"></i>
+            </button>
+        </div>
 
-            <ul>
-                <li><a class="mode-text" href="tarif.php">Tarif</a></li>
+        <!-- Logo -->
+        <div class="logo-brand mt-lg-5">
+            <a href="index.php">
+                <img src="img/logo/logo-listrik.png" alt="Logo" width="45" height="50"/>
+            </a>
+            <div>
+                <h6 class="title">Listrik Biru</h6>
+                <p class="tagline">Nyalakan Rumah Anda</p>
+            </div>
+        </div>
+
+        <!-- Menu -->
+        <hr/>
+        <nav class="menu flex-fill">
+            <div class="section-menu">
+                <a class="item-menu" href="tarif.php">Tarif</a>
                 <?php if($_SESSION['akun']['level'] == 'admin') { ?>
-                    <li><a class="mode-text" href="admin/transaksi.php">Transaksi</a></li>
-                    <li><a class="mode-text" href="admin/pelanggan.php">Pelanggan</a></li>
-                    <li><a class="mode-text" href="admin/daftar-pesan.php">Kontak</a></li>
-                <?php } if($_SESSION['akun']['level'] == 'user') { ?>
-                    <li><a class="mode-text" href="user/profil.php">Profil</a></li>
-                    <li><a class="mode-text" href="user/kontak.php">Kontak</a></li>
-                <?php } ?>
-                <div class="logout-btn">
-                    <a href="auth/logout.php">Logout</a>
-                </div>
-            </ul>
-            <div class="dark-mode-toggle">
-                <input type="checkbox" class="checkbox" id="chk"/>
-                <label class="label" for="chk">
-                    <i class="fas fa-moon"></i>
-                    <i class="fas fa-sun"></i>
-                    <div class="ball"></div>
-                </label>
-            </div>
+                    <a class="item-menu" href="admin/transaksi.php">Transaksi</a>
+                    <a class="item-menu" href="admin/pelanggan.php">Pelanggan</a>
+                    <a class="item-menu" href="admin/daftar-pesan.php">Kontak</a>
 
-            <div class="menu-toggle">
-                <input type="checkbox" id="menTog"/>
-                <span></span>
-                <span></span>
-                <span></span>
+                <?php } if($_SESSION['akun']['level'] == 'user') { ?>
+                    <a class="item-menu" href="user/profil.php">Profil</a>
+                    <a class="item-menu" href="user/kontak.php">Kontak</a>
+                <?php } ?>
             </div>
         </nav>
-        <!-- end navbar -->
-    </header>
+
+        <!-- FOOTER -->
+        <footer>
+            <div class="logout-btn">
+                <a href="auth/logout.php">Logout</a>
+            </div>
+            <p class="Keterangan" >Projek Akhir Pemrograman Web</p>
+        </footer>
+        <!-- END FOOTER -->
+    </aside>
+    <!-- End Sidebar -->
     <!-- END HEADER -->
 	
     <!-- TARIF CONTENT -->
-    <div class="container minggirin-navbar">
-        <div class="grid-container">
+    <section id="tarif-admin">
+        <div class="container mt-4 d-flex justify-content-center">
+            <div class="row w-75">
             <?php
                 require('php/connection.php');
-				$sql="SELECT * FROM tarif";
-				$query= mysqli_query($conn, $sql);
-				while ($data = mysqli_fetch_array($query)) {
-			?>
-                <div>
-                    <img src="img/voltase/<?php echo $data['foto'] ?>" onclick="window.location='tarif/tambah.php'"/>
-                    <p>
-                        <?php echo "$data[id]"; ?> ||
-                        <?php echo "$data[daya]"; ?><br>
-                    </p>
-                    <p>
-                        Rp.<?php echo "$data[tarifperkwh]"; ?>
-                    </p>
-                    <?php if($_SESSION['akun']['level'] == 'admin') { ?>
-                        <p class="aksi">
-                            <a class="edit" href="tarif/edit.php?id=<?php echo"$data[id]"; ?>">EDIT</a>
-                            <a class="hapus" href="tarif/aksi/delete.php?id=<?php echo"$data[id]"; ?>" onclick="return confirm('YAKINNN !!!')">HAPUS</a>
-                        </p>
-                    <?php } ?>
-                </div>
-			<?php } if($_SESSION['akun']['level'] == 'admin') { ?>
-                <div class="add-card">
-                    <div class="add-img" onclick="window.location='tarif/tambah.php'">
-                        <img src="img/voltase/add.jpg"/>
+
+                $sql = mysqli_query($conn, "SELECT * FROM tarif");
+                while ($data = mysqli_fetch_array($sql)){ 
+            ?>
+                <div class="col-3 mb-4">
+                    <div class="card shadow" style="width: 11.30rem; height: 13rem; border: 0;">
+                        <div class="card-tarif card-body text-center">
+                            <h5 class="card-title text-primary"><b><?php echo $data['id'] ?></b></h5>
+                            <span class="material-icons text-primary mt-4 mb-4">bolt</span>
+                            <ul class="list-inline text-primary">
+                                <li>Daya <strong><?php echo $data['daya'] ?></strong></li>
+                                <li>Tarif/Kwh <strong><?php echo $data['tarifperkwh'] ?></strong></li>
+                            </ul>
+                            <div class="d-flex flex-row">
+                                <a href="#" class="admin-aksi-tarif btn btn-primary w-50">Edit</a>
+                                <a href="#" class="admin-aksi-tarif btn btn-danger w-50">Hapus</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             <?php } ?>
+                <div class="col-3 mb-4">
+                    <div class="card shadow" style="width: 11.30rem; height: 13rem; border: 0;">
+                        <div class="card-body d-flex flex-column justify-content-center">
+                            <i class="btn fa-solid fa-plus text-primary fs-1 mb-3"></i>
+                            <a href="#" class="btn stretched-link text-primary"><b>Tambah Tarif</b></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    </section>
     <!-- END TARIF CONTENT -->
     
     <!-- FOOTER -->
-    <div style="border: 1px solid white; margin-top: 225px;"></div>
+    <!-- <div style="border: 1px solid white; margin-top: 225px;"></div>
     <footer class="mode-bg" style="position: static">
         <div class="footer-container">
             <div class="footer-title" id="contact">
@@ -126,7 +138,7 @@
                 <div class="footer-item">
                     <h4>Find Us On</h4>
                     <div class="circle-container">
-                        <!-- salah satu fitur pop up box (confirm) -->
+                        salah satu fitur pop up box (confirm)
                         <div class="circle ig">
                             <a href="https://www.instagram.com/pixel" onclick="return confirm('You will be redirected to other website.');"><i class="fa-brands fa-instagram"></i></a>
                         </div>
@@ -143,7 +155,7 @@
                 </div>
             </div>
         </div>
-    </footer>
+    </footer> -->
     <!-- END FOOTER -->
 
     <!-- javascript -->
